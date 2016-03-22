bd6e032d6488f5835cd52b8297af4bf3086e11dc
NIrav Patel <nap_38ce@yahoo.com>
2016-03-21 21:06:29 -0400

    Now sending 32bit float for image mesage instead of unsigned int 16.

--- src/OpenIGTLinkMessageSender.m
+++ src/OpenIGTLinkMessageSender.m
@@ -51,22 +51,23 @@ function result=igtlSendImageMessage(deviceName, RI, RJ, RK , TX, TY, TZ, SX, SY
     % note that it is an unsigned short value, but small positive signed and unsigned numbers are represented the same way, so we can use writeShort
     msg.body = [convertFromUint16ToUint8Vector(1),... %Version
         uint8(1), ... %Number of Image Components (1:Scalar, >1:Vector). (NOTE: Vector data is stored fully interleaved.)
-        uint8(5), ... %Scalar type (2:int8 3:uint8 4:int16 5:uint16 6:int32 7:uint32 10:float32 11:float64)
+        uint8(10), ... %Scalar type (2:int8 3:uint8 4:int16 5:uint16 6:int32 7:uint32 10:float32 11:float64)
         uint8(2), ... %Endian for image data (1:BIG 2:LITTLE) (NOTE: values in image header is fixed to BIG endian)
         uint8(1), ... % image coordinate (1:RAS 2:LPS)
-        convertFromUint16ToUint8Vector(uint16(RI)), convertFromUint16ToUint8Vector(uint16(RJ)), convertFromUint16ToUint8Vector(uint16(RK)), ... %Number of pixels in each direction
+        convertFromUint16ToUint8Vector(RI), convertFromUint16ToUint8Vector(RJ), convertFromUint16ToUint8Vector(RK), ... %Number of pixels in each direction
         convertFromFloat32ToUint8Vector(TX),convertFromFloat32ToUint8Vector(TY), convertFromFloat32ToUint8Vector(TZ) ... %Transverse vector (direction for 'i' index) / The length represents pixel size in 'i' direction in millimeter
         convertFromFloat32ToUint8Vector(SX),convertFromFloat32ToUint8Vector(SY), convertFromFloat32ToUint8Vector(SZ) ... %Transverse vector (direction for 'j' index) / The length represents pixel size in 'j' direction in millimeter
         convertFromFloat32ToUint8Vector(NX),convertFromFloat32ToUint8Vector(NY), convertFromFloat32ToUint8Vector(NZ) ... %Normal vector of image plane(direction for 'k' index) / The length represents pixel size in 'z' direction or slice thickness in millimeter
         convertFromFloat32ToUint8Vector(PX),convertFromFloat32ToUint8Vector(PY), convertFromFloat32ToUint8Vector(PZ) ... %center position of the image (in millimeter) (*)
-        convertFromUint16ToUint8Vector(uint16(0)), convertFromUint16ToUint8Vector(uint16(0)), convertFromUint16ToUint8Vector(uint16(0)), ... %Number of pixels in each direction
-        convertFromUint16ToUint8Vector(uint16(RI)), convertFromUint16ToUint8Vector(uint16(RJ)), convertFromUint16ToUint8Vector(uint16(RK)) %Number of pixels in each direction
+        convertFromUint16ToUint8Vector(0), convertFromUint16ToUint8Vector(0), convertFromUint16ToUint8Vector(0), ... %Number of pixels in each direction
+        convertFromUint16ToUint8Vector(RI), convertFromUint16ToUint8Vector(RJ), convertFromUint16ToUint8Vector(RK) %Number of pixels in each direction
         ];
     for i=1:size(imageData,1)
         for j=1:size(imageData,2)
-            msg.body=[ msg.body, convertFromUint16ToUint8Vector(uint16(imageData(i,j)))];
+            msg.body=[ msg.body, convertFromFloat32ToUint8Vector(imageData(i,j))];
         end
     end
+    disp(['Size of the Image Message Body=', num2str(length(uint8(msg.body)))]);
     result=WriteOpenIGTLinkMessage(msg);
 end
 
@@ -79,6 +80,8 @@ function result=WriteOpenIGTLinkMessage(msg)
     % Add constant fields values
     msg.versionNumber=1;
     msg.bodySize=length(msg.body);
+    disp(['Msg.BodySize=', num2str(msg.bodySize)]);
+
     msg.bodyCrc=0; % TODO: compute this
     % Pack message
     data=[];
@@ -91,7 +94,9 @@ function result=WriteOpenIGTLinkMessage(msg)
     data=[data, uint8(msg.body)];    
     result=1;
     try
+        disp(['Length Of Data being writeen to socket=', num2str(length(data))]);
         DataOutputStream(socket.outputStream).write(uint8(data),0,length(data));
+        DataOutputStream(socket.outputStream).flush;
     catch ME
         disp(ME.message)
         result=0;
@@ -169,4 +174,9 @@ function timestamp = igtlTimestampNow()
 %
 
 timestamp = java.lang.System.currentTimeMillis/1000;
+end
+function calcCRC64()
+poly = [64,62,57,55,54,53,52,47,46,45,40,39,38,37,35,33,32,31,29,27,24,23,22,21,19,17,13,12,10,9,7,4,1,0];
+H = comm.CRCGenerator('Polynomial', poly);
+
 end
